5319b9445246325d7a9928329c1d13aafb301c84
Vince <vincedimatatac@gmail.com>
2026-09-01 00:11:42 +0800

    chore: configure env adn cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,20 @@
 import express from "express";
 import healthRouter from "./routes/health.routes";
+import cors from "cors";
+import "dotenv/config";
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  }),
+);
 
 app.use("/api", healthRouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
