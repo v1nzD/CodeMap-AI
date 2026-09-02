007c1672971764a32f7f7fd308146c68667e65ab
Vince <vincedimatatac@gmail.com>
2026-09-02 18:21:04 +0800

    added CRUD MVC for repositories

--- server/src/routes/repositories.routes.ts
+++ server/src/routes/repositories.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { createRepo } from "../controllers/repository.controller";
+import {
+  createRepo,
+  deleteRepo,
+  getAllRepo,
+  getRepoById,
+} from "../controllers/repository.controller";
 
 const repoRouter = Router();
 
 repoRouter.post("/", createRepo);
+repoRouter.get("/", getAllRepo);
+repoRouter.get("/:id", getRepoById);
+repoRouter.delete("/:id", deleteRepo);
 
 export default repoRouter;
--- server/src/services/repositories/repositories.service.ts
+++ server/src/services/repositories/repositories.service.ts
@@ -13,3 +13,28 @@ export async function createRepository(
 
   return result.rows[0];
 }
+
+export async function getAllRepositories() {
+  const result = await pool.query(
+    "SELECT * FROM repositories ORDER BY created_at DESC",
+  );
+
+  return result.rows;
+}
+
+export async function getRepositoryById(id: number) {
+  const result = await pool.query("SELECT * FROM repositories WHERE id = $1", [
+    id,
+  ]);
+
+  return result.rows[0];
+}
+
+export async function deleteRepository(id: number) {
+  const result = await pool.query(
+    "DELETE FROM repositories WHERE id = $1 RETURNING *",
+    [id],
+  );
+
+  return result.rows[0];
+}
