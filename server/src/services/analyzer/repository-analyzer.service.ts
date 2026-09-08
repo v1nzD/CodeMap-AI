60af3faeac28967ab92320ca4c47052b34b846a0
Vince <vincedimatatac@gmail.com>
2026-09-08 21:53:46 +0800

    connected persistence service to analyzeGithubRepository pipeline

--- server/src/services/analyzer/repository-analyzer.service.ts
+++ server/src/services/analyzer/repository-analyzer.service.ts
@@ -2,31 +2,40 @@ import {
   getGithubFileContent,
   getGithubFileTree,
 } from "../github/github.service";
+
 import { analyzeRepository } from "./analyzer.service";
+import { saveAnalyzedRepository } from "../repositories/repositories-persistence.service";
 
 const extensions = [".ts", ".tsx", ".js", ".jsx"];
 
-export async function analyzeGithubRepository(owner: string, name: string) {
-  // get github tree
+export async function analyzeGithubRepository(
+  owner: string,
+  name: string,
+  repositoryId: number,
+) {
   const githubTree = await getGithubFileTree(owner, name);
 
-  // keep only supported files
   const sourceFiles = githubTree.tree.filter(
     (file) =>
       file.type === "blob" &&
       extensions.some((extension) => file.path.endsWith(extension)),
   );
 
-  // retrieve contents of each file
-  // requests happen concurrently => waits for all the files
   const files = await Promise.all(
     sourceFiles.map(async (file) => {
       const content = await getGithubFileContent(owner, name, file.path);
 
-      return { path: file.path, content, type: file.type };
+      return {
+        path: file.path,
+        content,
+        type: file.type,
+      };
     }),
   );
 
-  // analzye the entire repository
-  return analyzeRepository(files);
+  const analysis = await analyzeRepository(files);
+
+  await saveAnalyzedRepository(repositoryId, analysis);
+
+  return analysis;
 }
